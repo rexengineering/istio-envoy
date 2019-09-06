rexengineering/istio-envoy

commit ffffcd327b1d78e871272bd182a4020b1d6dcd8d

Exporting platform_impl_lib headers (#8045)
This allows consuming projects using repository overlaying to disambiguate overlapping include paths when it comes to platform_impl.h by going through envoy/external/...

Addendum to #8005

Risk Level: Low
Testing: N/A

Signed-off-by: Otto van der Schaaf <oschaaf@we-amp.com>

source/exe/platform_impl.h
@@ -1,0 +1,19 @@
+#pragma once
+
+#include "envoy/filesystem/filesystem.h"
+#include "envoy/thread/thread.h"
+
+namespace Envoy {
+
+class PlatformImpl {
+public:
+  PlatformImpl();
+  Thread::ThreadFactory& threadFactory() { return *thread_factory_; }
+  Filesystem::Instance& fileSystem() { return *file_system_; }
+
+private:
+  std::unique_ptr<Thread::ThreadFactory> thread_factory_;
+  std::unique_ptr<Filesystem::Instance> file_system_;
+};
+
+} // namespace Envoy

source/exe/posix/platform_impl.cc
@@ -1,0 +1,12 @@
+#include "common/common/thread_impl.h"
+#include "common/filesystem/filesystem_impl.h"
+
+#include "exe/platform_impl.h"
+
+namespace Envoy {
+
+PlatformImpl::PlatformImpl()
+    : thread_factory_(std::make_unique<Thread::ThreadFactoryImplPosix>()),
+      file_system_(std::make_unique<Filesystem::InstanceImplPosix>()) {}
+
+} // namespace Envoy

source/exe/win32/platform_impl.cc
@@ -1,0 +1,26 @@
+#include "common/common/assert.h"
+#include "common/common/thread_impl.h"
+#include "common/filesystem/filesystem_impl.h"
+
+#include "exe/platform_impl.h"
+
+// clang-format off
+#include <winsock2.h>
+// clang-format on
+
+namespace Envoy {
+
+PlatformImpl::PlatformImpl()
+    : thread_factory_(std::make_unique<Thread::ThreadFactoryImplWin32>()),
+      file_system_(std::make_unique<Filesystem::InstanceImplWin32>()) {
+  const WORD wVersionRequested = MAKEWORD(2, 2);
+  WSADATA wsaData;
+  const int rc = ::WSAStartup(wVersionRequested, &wsaData);
+  RELEASE_ASSERT(rc == 0, "WSAStartup failed with error");
+}
+
+~PlatformImpl() { ::WSACleanup(); }
+
+}; // namespace Envoy
+
+} // namespace Envoy

source/exe/posix/platform_impl.h
@@ -1,18 +1,0 @@
-#pragma once
-
-#include "common/common/thread_impl.h"
-#include "common/filesystem/filesystem_impl.h"
-
-namespace Envoy {
-
-class PlatformImpl {
-public:
-  Thread::ThreadFactory& threadFactory() { return thread_factory_; }
-  Filesystem::Instance& fileSystem() { return file_system_; }
-
-private:
-  Thread::ThreadFactoryImplPosix thread_factory_;
-  Filesystem::InstanceImplPosix file_system_;
-};
-
-} // namespace Envoy

source/exe/win32/platform_impl.h
@@ -1,32 +1,0 @@
-#pragma once
-
-#include "common/common/assert.h"
-#include "common/common/thread_impl.h"
-#include "common/filesystem/filesystem_impl.h"
-
-// clang-format off
-#include <winsock2.h>
-// clang-format on
-
-namespace Envoy {
-
-class PlatformImpl {
-public:
-  PlatformImpl() {
-    const WORD wVersionRequested = MAKEWORD(2, 2);
-    WSADATA wsaData;
-    const int rc = ::WSAStartup(wVersionRequested, &wsaData);
-    RELEASE_ASSERT(rc == 0, "WSAStartup failed with error");
-  }
-
-  ~PlatformImpl() { ::WSACleanup(); }
-
-  Thread::ThreadFactory& threadFactory() { return thread_factory_; }
-  Filesystem::Instance& fileSystem() { return file_system_; }
-
-private:
-  Thread::ThreadFactoryImplWin32 thread_factory_;
-  Filesystem::InstanceImplWin32 file_system_;
-};
-
-} // namespace Envoy

source/exe/BUILD
@@ -57,7 +57,8 @@ envoy_cc_library(
     ],
     deps = [
         ":envoy_main_common_lib",
-    ] + envoy_cc_platform_dep("platform_impl_lib"),
+        ":platform_impl_lib",
+    ],
 )
 
 envoy_cc_library(
@@ -66,6 +67,7 @@ envoy_cc_library(
     hdrs = ["main_common.h"],
     deps = [
         ":envoy_common_lib",
+        ":platform_impl_lib",
         ":process_wide_lib",
         "//source/common/api:os_sys_calls_lib",
         "//source/common/common:compiler_requirements_lib",
@@ -80,7 +82,7 @@ envoy_cc_library(
             "//source/common/signal:sigaction_lib",
             ":terminate_handler_lib",
         ],
-    }) + envoy_cc_platform_dep("platform_impl_lib"),
+    }),
 )
 
 envoy_cc_library(
@@ -96,11 +98,26 @@ envoy_cc_library(
     ] + envoy_google_grpc_external_deps(),
 )
 
+envoy_cc_library(
+    name = "platform_impl_lib",
+    deps = [":platform_header_lib"] +
+           envoy_cc_platform_dep("platform_impl_lib"),
+)
+
+envoy_cc_library(
+    name = "platform_header_lib",
+    hdrs = ["platform_impl.h"],
+    deps = [
+        "//include/envoy/filesystem:filesystem_interface",
+        "//include/envoy/thread:thread_interface",
+    ],
+)
+
 envoy_cc_posix_library(
     name = "platform_impl_lib",
-    hdrs = ["posix/platform_impl.h"],
-    strip_include_prefix = "posix",
+    srcs = ["posix/platform_impl.cc"],
     deps = [
+        ":platform_header_lib",
         "//source/common/common:thread_lib",
         "//source/common/filesystem:filesystem_lib",
     ],
@@ -108,9 +125,9 @@ envoy_cc_posix_library(
 
 envoy_cc_win32_library(
     name = "platform_impl_lib",
-    hdrs = ["win32/platform_impl.h"],
-    strip_include_prefix = "win32",
+    srcs = ["win32/platform_impl.cc"],
     deps = [
+        ":platform_header_lib",
         "//source/common/common:assert_lib",
         "//source/common/common:thread_lib",
         "//source/common/filesystem:filesystem_lib",

commit 8663198501c6520eb6ff65c4c3d285880df1c682

fix windows implementation of PlatformImpl (#8169)
Add missing destructor to class declaration.
Fix copy/paste errors.
These errors were apparently introduced in ffffcd3.

Risk Level: Low
Testing: Passed Windows testing locally
Docs Changes: n/a
Release Notes: n/a

Signed-off-by: William Rowe wrowe@pivotal.io
Signed-off-by: Yechiel Kalmenson <ykalmenson@pivotal.io>

source/exe/platform_impl.h
@@ -8,6 +8,7 @@ namespace Envoy {
 class PlatformImpl {
 public:
   PlatformImpl();
+  ~PlatformImpl();
   Thread::ThreadFactory& threadFactory() { return *thread_factory_; }
   Filesystem::Instance& fileSystem() { return *file_system_; }
 

source/exe/posix/platform_impl.cc
@@ -9,4 +9,6 @@ PlatformImpl::PlatformImpl()
     : thread_factory_(std::make_unique<Thread::ThreadFactoryImplPosix>()),
       file_system_(std::make_unique<Filesystem::InstanceImplPosix>()) {}
 
+PlatformImpl::~PlatformImpl() = default;
+
 } // namespace Envoy

source/exe/win32/platform_impl.cc
@@ -19,8 +19,6 @@ PlatformImpl::PlatformImpl()
   RELEASE_ASSERT(rc == 0, "WSAStartup failed with error");
 }
 
-~PlatformImpl() { ::WSACleanup(); }
-
-}; // namespace Envoy
+PlatformImpl::~PlatformImpl() { ::WSACleanup(); }
 
 } // namespace Envoy