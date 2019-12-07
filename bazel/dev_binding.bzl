rexengineering/istio-envoy

commit e85205f65939a88db53226584931b4ae84c8bd77

tools: add support for Clang libtooling Envoy developer tools. (#8597)
This is intended to support libtooling based tools, e.g. for automating
API major version upgrades, linting for security properties, enhanced
domain specific format or presubmit restrictions, etc.

See tools/clang_tools/README.md for further details.

Part of https://github.com/envoyproxy/envoy/issues/8082.

Risk level: Low
Testing: syntax_only Hello World-style example works.

Signed-off-by: Harvey Tuch <htuch@google.com>

bazel/repositories.bzl
@@ -89,6 +89,27 @@ def _go_deps(skip_targets):
         _repository_impl("io_bazel_rules_go")
         _repository_impl("bazel_gazelle")
 
+def _clang_tools_impl(ctxt):
+    if "LLVM_CONFIG" in ctxt.os.environ:
+        llvm_config_path = ctxt.os.environ["LLVM_CONFIG"]
+        exec_result = ctxt.execute([llvm_config_path, "--includedir"])
+        if exec_result.return_code != 0:
+            fail(llvm_config_path + " --includedir returned %d" % exec_result.return_code)
+        clang_tools_include_path = exec_result.stdout.rstrip()
+        exec_result = ctxt.execute([llvm_config_path, "--libdir"])
+        if exec_result.return_code != 0:
+            fail(llvm_config_path + " --libdir returned %d" % exec_result.return_code)
+        clang_tools_lib_path = exec_result.stdout.rstrip()
+        for include_dir in ["clang", "clang-c", "llvm", "llvm-c"]:
+            ctxt.symlink(clang_tools_include_path + "/" + include_dir, include_dir)
+        ctxt.symlink(clang_tools_lib_path, "lib")
+        ctxt.symlink(Label("//tools/clang_tools/support:BUILD.prebuilt"), "BUILD")
+
+_clang_tools = repository_rule(
+    implementation = _clang_tools_impl,
+    environ = ["LLVM_CONFIG"],
+)
+
 def envoy_dependencies(skip_targets = []):
     # Treat Envoy's overall build config as an external repo, so projects that
     # build Envoy as a subcomponent can easily override the config.
@@ -108,6 +129,8 @@ def envoy_dependencies(skip_targets = []):
         name = "ssl",
         actual = "@envoy//bazel:boringssl",
     )
+
+    _clang_tools(name = "clang_tools")
 
     # The long repo names (`com_github_fmtlib_fmt` instead of `fmtlib`) are
     # semi-standard in the Bazel community, intended to avoid both duplicate

tools/clang_tools/syntax_only/BUILD
@@ -1,0 +1,13 @@
+load("//tools/clang_tools/support:clang_tools.bzl", "envoy_clang_tools_cc_binary")
+
+licenses(["notice"])  # Apache 2
+
+envoy_clang_tools_cc_binary(
+    name = "syntax_only",
+    srcs = ["main.cc"],
+    deps = [
+        "@clang_tools//:clang_astmatchers",
+        "@clang_tools//:clang_basic",
+        "@clang_tools//:clang_tooling",
+    ],
+)

tools/clang_tools/support/BUILD
@@ -1,0 +1,1 @@
+licenses(["notice"])  # Apache 2

.bazelrc
@@ -27,9 +27,10 @@ build:linux --copt=-fPIC
 # We already have absl in the build, define absl=1 to tell googletest to use absl for backtrace.
 build --define absl=1
 
-# Pass PATH, CC and CXX variables from the environment.
+# Pass PATH, CC, CXX and LLVM_CONFIG variables from the environment.
 build --action_env=CC
 build --action_env=CXX
+build --action_env=LLVM_CONFIG
 build --action_env=PATH
 
 # Common flags for sanitizers

bazel/setup_clang.sh
@@ -17,6 +17,7 @@ echo "# Generated file, do not edit. If you want to disable clang, just delete t
 build:clang --action_env=PATH=${PATH}
 build:clang --action_env=CC=clang
 build:clang --action_env=CXX=clang++
+build:clang --action_env=LLVM_CONFIG=${LLVM_PREFIX}/bin/llvm-config
 build:clang --linkopt=-L$(llvm-config --libdir)
 build:clang --linkopt=-Wl,-rpath,$(llvm-config --libdir)
 

ci/run_clang_tidy.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 
 ENVOY_SRCDIR=${ENVOY_SRCDIR:-$(cd $(dirname $0)/.. && pwd)}
 
-LLVM_CONFIG=${LLVM_CONFIG:-llvm-config}
+export LLVM_CONFIG=${LLVM_CONFIG:-llvm-config}
 LLVM_PREFIX=${LLVM_PREFIX:-$(${LLVM_CONFIG} --prefix)}
 CLANG_TIDY=${CLANG_TIDY:-$(${LLVM_CONFIG} --bindir)/clang-tidy}
 CLANG_APPLY_REPLACEMENTS=${CLANG_APPLY_REPLACEMENTS:-$(${LLVM_CONFIG} --bindir)/clang-apply-replacements}

tools/check_format.py
@@ -860,7 +860,10 @@ if __name__ == "__main__":
   namespace_check = args.namespace_check
   namespace_check_excluded_paths = args.namespace_check_excluded_paths
   build_fixer_check_excluded_paths = args.build_fixer_check_excluded_paths + [
-      "./bazel/external/", "./bazel/toolchains/", "./bazel/BUILD"
+      "./bazel/external/",
+      "./bazel/toolchains/",
+      "./bazel/BUILD",
+      "./tools/clang_tools",
   ]
   include_dir_order = args.include_dir_order
   if args.add_excluded_prefixes:

tools/clang_tools/support/BUILD.prebuilt
@@ -1,0 +1,398 @@
+# Clang 9.0 library pre-built Bazel.
+#
+# This file was mostly manually assembled (with some hacky Python scripts) from
+# clang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz and corresponding
+# https://github.com/llvm/llvm-project.git source. It needs Clang 9.0 to work.
+#
+# The BUILD file has sufficient dependency relationships
+# between the prebuilt libraries in a clang-llvm distribution to support building libtooling
+# based binaries in the Envoy repository. We're chasing a moving target and as new libraries are
+# depended upon in new Clang versions, it will be necessary to augment these definitions.
+#
+# The key to understanding llvm-project layout is that there are a collection of libraries in
+# {clang,llvm}/lib. For the clang libraries, the CMakeLists.txt supplies the Clang library deps in
+# LINK_LIBS inside add_clang_library() and the llvm deps in LLVM_LINK_COMPONENTS. For the llvm
+# libraries, LLVMBuild.txt provides llvm deps (it does not reference any of the clang libs).
+#
+# It's kind of terrible that we need to do this by hand, but llvm-project is CMake canonical, and we
+# don't want to use rules_foreign_cc to build the libraries from source just to access some
+# developer libs which will exist on the filesystem of most devs who are using Clang.
+
+package(default_visibility = ["//visibility:public"])
+
+# We should use cc_import below, but it doesn't like .def files in Clang. See
+# https://github.com/bazelbuild/bazel/issues/6767.
+#
+
+cc_library(
+    name = "clang_analysis",
+    srcs = ["lib/libclangAnalysis.a"],
+    hdrs = glob(["clang/Analysis/**"]),
+    deps = [
+        ":clang_ast",
+        ":clang_astmatchers",
+        ":clang_basic",
+        ":clang_lex",
+        ":llvm_support",
+    ],
+)
+
+cc_library(
+    name = "clang_basic",
+    srcs = ["lib/libclangBasic.a"],
+    hdrs = glob([
+        "clang/Basic/**",
+        "clang-c/**",
+    ]),
+    deps = [
+        ":llvm_core",
+        ":llvm_mc",
+        ":llvm_support",
+        ":llvm_target",
+    ],
+)
+
+cc_library(
+    name = "clang_ast",
+    srcs = ["lib/libclangAST.a"],
+    hdrs = glob(["clang/AST/**"]),
+    deps = [
+        ":clang_basic",
+        ":clang_lex",
+        ":llvm_binaryformat",
+        ":llvm_core",
+        ":llvm_support",
+    ],
+)
+
+cc_library(
+    name = "clang_astmatchers",
+    srcs = ["lib/libclangASTMatchers.a"],
+    hdrs = glob(["clang/ASTMatchers/**"]),
+    deps = [
+        ":clang_ast",
+        ":clang_basic",
+        ":llvm_support",
+    ],
+)
+
+cc_library(
+    name = "clang_driver",
+    srcs = ["lib/libclangDriver.a"],
+    hdrs = glob(["clang/Driver/**"]),
+    deps = [
+        ":clang_basic",
+        ":llvm_binaryformat",
+        ":llvm_option",
+        ":llvm_support",
+    ],
+)
+
+cc_library(
+    name = "clang_edit",
+    srcs = ["lib/libclangEdit.a"],
+    hdrs = glob(["clang/Edit/**"]),
+    deps = [
+        ":clang_ast",
+        ":clang_basic",
+        ":clang_lex",
+        ":llvm_support",
+    ],
+)
+
+cc_library(
+    name = "clang_format",
+    srcs = ["lib/libclangFormat.a"],
+    hdrs = glob(["clang/Format/**"]),
+    deps = [
+        ":clang_basic",
+        ":clang_lex",
+        ":clang_toolingcore",
+        ":clang_toolinginclusions",
+        ":llvm_support",
+    ],
+)
+
+cc_library(
+    name = "clang_frontend",
+    srcs = ["lib/libclangFrontend.a"],
+    hdrs = glob(["clang/Frontend/**"]),
+    deps = [
+        ":clang_ast",
+        ":clang_basic",
+        ":clang_driver",
+        ":clang_edit",
+        ":clang_lex",
+        ":clang_parse",
+        ":clang_sema",
+        ":clang_serialization",
+        ":llvm_bitreader",
+        ":llvm_option",
+        ":llvm_profiledata",
+        ":llvm_support",
+    ],
+)
+
+cc_library(
+    name = "clang_lex",
+    srcs = ["lib/libclangLex.a"],
+    hdrs = glob(["clang/Lex/**"]),
+    deps = [
+        ":clang_basic",
+        ":llvm_support",
+    ],
+)
+
+cc_library(
+    name = "clang_parse",
+    srcs = ["lib/libclangParse.a"],
+    hdrs = glob(["clang/Parse/**"]),
+    deps = [
+        ":clang_ast",
+        ":clang_basic",
+        ":clang_lex",
+        ":clang_sema",
+        ":llvm_mc",
+        ":llvm_mcparser",
+        ":llvm_support",
+    ],
+)
+
+cc_library(
+    name = "clang_rewrite",
+    srcs = ["lib/libclangRewrite.a"],
+    hdrs = glob(["clang/Rewrite/**"]),
+    deps = [
+        ":clang_basic",
+        ":clang_lex",
+        ":llvm_support",
+    ],
+)
+
+cc_library(
+    name = "clang_sema",
+    srcs = ["lib/libclangSema.a"],
+    hdrs = glob(["clang/Sema/**"]),
+    deps = [
+        ":clang_analysis",
+        ":clang_ast",
+        ":clang_basic",
+        ":clang_edit",
+        ":clang_lex",
+        ":llvm_support",
+    ],
+)
+
+cc_library(
+    name = "clang_serialization",
+    srcs = ["lib/libclangSerialization.a"],
+    hdrs = glob(["clang/Serialization/**"]),
+    deps = [
+        ":clang_ast",
+        ":clang_basic",
+        ":clang_lex",
+        ":clang_sema",
+        ":llvm_bitreader",
+        ":llvm_bitstreamreader",
+        ":llvm_support",
+    ],
+)
+
+cc_library(
+    name = "clang_tooling",
+    srcs = ["lib/libclangTooling.a"],
+    hdrs = glob(["clang/Tooling/**"]),
+    deps = [
+        ":clang_ast",
+        ":clang_astmatchers",
+        ":clang_basic",
+        ":clang_driver",
+        ":clang_format",
+        ":clang_frontend",
+        ":clang_lex",
+        ":clang_rewrite",
+        ":clang_serialization",
+        ":clang_toolingcore",
+        ":llvm_option",
+        ":llvm_support",
+    ],
+)
+
+cc_library(
+    name = "clang_toolingcore",
+    srcs = ["lib/libclangToolingCore.a"],
+    hdrs = glob(["clang/Tooling/Core/**"]),
+    deps = [
+        ":clang_ast",
+        ":clang_basic",
+        ":clang_lex",
+        ":clang_rewrite",
+        ":llvm_support",
+    ],
+)
+
+cc_library(
+    name = "clang_toolinginclusions",
+    srcs = ["lib/libclangToolingInclusions.a"],
+    hdrs = glob(["clang/Tooling/Inclusions/**"]),
+    deps = [
+        ":clang_basic",
+        ":clang_lex",
+        ":clang_rewrite",
+        ":clang_toolingcore",
+        ":llvm_support",
+    ],
+)
+
+cc_library(
+    name = "llvm_analysis",
+    srcs = ["lib/libLLVMAnalysis.a"],
+    hdrs = glob(["llvm/Analysis/**"]),
+    deps = [
+        ":llvm_binaryformat",
+        ":llvm_core",
+        ":llvm_object",
+        ":llvm_profiledata",
+        ":llvm_support",
+    ],
+)
+
+cc_library(
+    name = "llvm_binaryformat",
+    srcs = ["lib/libLLVMBinaryFormat.a"],
+    hdrs = glob(["llvm/BinaryFormat/**"]),
+    deps = [":llvm_support"],
+)
+
+cc_library(
+    name = "llvm_bitreader",
+    srcs = ["lib/libLLVMBitReader.a"],
+    hdrs = glob(["llvm/Bitcode/**"]),
+    deps = [
+        ":llvm_bitstreamreader",
+        ":llvm_core",
+        ":llvm_support",
+    ],
+)
+
+cc_library(
+    name = "llvm_bitstreamreader",
+    srcs = ["lib/libLLVMBitstreamReader.a"],
+    hdrs = glob(["llvm/Bitstream/**"]),
+    deps = [
+        ":llvm_support",
+    ],
+)
+
+cc_library(
+    name = "llvm_core",
+    srcs = ["lib/libLLVMCore.a"],
+    hdrs = glob([
+        "llvm/ADT/**",
+        "llvm/IR/**",
+        "llvm/*",
+        "llvm-c/**",
+    ]),
+    deps = [
+        ":llvm_binaryformat",
+        ":llvm_remarks",
+        ":llvm_support",
+    ],
+)
+
+cc_library(
+    name = "llvm_demangle",
+    srcs = ["lib/libLLVMDemangle.a"],
+    hdrs = glob(["llvm/Demangle/**"]),
+)
+
+cc_library(
+    name = "llvm_mc",
+    srcs = ["lib/libLLVMMC.a"],
+    hdrs = glob(["llvm/MC/**"]),
+    deps = [
+        ":llvm_binaryformat",
+        ":llvm_support",
+    ],
+)
+
+cc_library(
+    name = "llvm_mcparser",
+    srcs = ["lib/libLLVMMCParser.a"],
+    hdrs = glob(["llvm/MC/MCParser/**"]),
+    deps = [
+        ":llvm_mc",
+        ":llvm_support",
+    ],
+)
+
+cc_library(
+    name = "llvm_object",
+    srcs = ["lib/libLLVMObject.a"],
+    hdrs = glob(["llvm/Object/**"]),
+    deps = [
+        ":llvm_binaryformat",
+        ":llvm_bitreader",
+        ":llvm_core",
+        ":llvm_mc",
+        ":llvm_mcparser",
+        ":llvm_support",
+    ],
+)
+
+cc_library(
+    name = "llvm_option",
+    srcs = ["lib/libLLVMOption.a"],
+    hdrs = glob(["llvm/Option/**"]),
+    deps = [
+        ":llvm_support",
+    ],
+)
+
+cc_library(
+    name = "llvm_profiledata",
+    srcs = ["lib/libLLVMProfileData.a"],
+    hdrs = glob(["llvm/ProfileData/**"]),
+    deps = [
+        ":llvm_core",
+        ":llvm_support",
+    ],
+)
+
+cc_library(
+    name = "llvm_remarks",
+    srcs = ["lib/libLLVMRemarks.a"],
+    hdrs = glob(["llvm/Remarks/**"]),
+    deps = [
+        ":llvm_support",
+    ],
+)
+
+cc_library(
+    name = "llvm_support",
+    srcs = ["lib/libLLVMSupport.a"],
+    hdrs = glob([
+        "llvm/Config/**",
+        "llvm/Support/**",
+    ]),
+    linkopts = [
+        "-lcurses",
+        "-lpthread",
+        "-lz",
+    ],
+    deps = [
+        ":llvm_demangle",
+    ],
+)
+
+cc_library(
+    name = "llvm_target",
+    srcs = ["lib/libLLVMTarget.a"],
+    hdrs = glob(["llvm/Target/**"]),
+    deps = [
+        ":llvm_analysis",
+        ":llvm_core",
+        ":llvm_mc",
+        ":llvm_support",
+    ],
+)

commit 2835f0516bfecbfdc05dd3d818d2faa906f7104f

tools: move clang_tools under @envoy_dev (#9249)
Fixes regression in gen_compilation_database introduced in #8597, this causes unpleasant errors if a developer has not configured LLVM_CONFIG. The solution is to move developer tools into an `@envoy_api` like local external repository called `@envoy_dev`.

Risk level: Low
Testing: Manual, with LLVM_CONFIG set/unset.

Signed-off-by: Harvey Tuch <htuch@google.com>

.bazelignore
@@ -1,2 +1,3 @@
 api
 examples/grpc-bridge/script
+tools/clang_tools

bazel/dev_binding.bzl
@@ -1,0 +1,40 @@
+def _default_envoy_dev_impl(ctxt):
+    if "LLVM_CONFIG" in ctxt.os.environ:
+        ctxt.file("WORKSPACE", "")
+        ctxt.file("BUILD.bazel", "")
+        ctxt.symlink(ctxt.path(ctxt.attr.envoy_root).dirname.get_child("tools").get_child("clang_tools"), "clang_tools")
+
+_default_envoy_dev = repository_rule(
+    implementation = _default_envoy_dev_impl,
+    attrs = {
+        "envoy_root": attr.label(default = "@envoy//:BUILD"),
+    },
+)
+
+def _clang_tools_impl(ctxt):
+    if "LLVM_CONFIG" in ctxt.os.environ:
+        llvm_config_path = ctxt.os.environ["LLVM_CONFIG"]
+        exec_result = ctxt.execute([llvm_config_path, "--includedir"])
+        if exec_result.return_code != 0:
+            fail(llvm_config_path + " --includedir returned %d" % exec_result.return_code)
+        clang_tools_include_path = exec_result.stdout.rstrip()
+        exec_result = ctxt.execute([llvm_config_path, "--libdir"])
+        if exec_result.return_code != 0:
+            fail(llvm_config_path + " --libdir returned %d" % exec_result.return_code)
+        clang_tools_lib_path = exec_result.stdout.rstrip()
+        for include_dir in ["clang", "clang-c", "llvm", "llvm-c"]:
+            ctxt.symlink(clang_tools_include_path + "/" + include_dir, include_dir)
+        ctxt.symlink(clang_tools_lib_path, "lib")
+        ctxt.symlink(Label("@envoy_dev//clang_tools/support:BUILD.prebuilt"), "BUILD")
+
+_clang_tools = repository_rule(
+    implementation = _clang_tools_impl,
+    environ = ["LLVM_CONFIG"],
+)
+
+def envoy_dev_binding():
+    # Treat the Envoy developer tools that require llvm as an external repo, this avoids
+    # breaking bazel build //... when llvm is not installed.
+    if "envoy_dev" not in native.existing_rules().keys():
+        _default_envoy_dev(name = "envoy_dev")
+        _clang_tools(name = "clang_tools")

bazel/repositories.bzl
@@ -1,4 +1,5 @@
 load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
+load(":dev_binding.bzl", "envoy_dev_binding")
 load(":genrule_repository.bzl", "genrule_repository")
 load("@envoy_api//bazel:envoy_http_archive.bzl", "envoy_http_archive")
 load(":repository_locations.bzl", "REPOSITORY_LOCATIONS")
@@ -89,28 +90,10 @@ def _go_deps(skip_targets):
         _repository_impl("io_bazel_rules_go")
         _repository_impl("bazel_gazelle")
 
-def _clang_tools_impl(ctxt):
-    if "LLVM_CONFIG" in ctxt.os.environ:
-        llvm_config_path = ctxt.os.environ["LLVM_CONFIG"]
-        exec_result = ctxt.execute([llvm_config_path, "--includedir"])
-        if exec_result.return_code != 0:
-            fail(llvm_config_path + " --includedir returned %d" % exec_result.return_code)
-        clang_tools_include_path = exec_result.stdout.rstrip()
-        exec_result = ctxt.execute([llvm_config_path, "--libdir"])
-        if exec_result.return_code != 0:
-            fail(llvm_config_path + " --libdir returned %d" % exec_result.return_code)
-        clang_tools_lib_path = exec_result.stdout.rstrip()
-        for include_dir in ["clang", "clang-c", "llvm", "llvm-c"]:
-            ctxt.symlink(clang_tools_include_path + "/" + include_dir, include_dir)
-        ctxt.symlink(clang_tools_lib_path, "lib")
-        ctxt.symlink(Label("//tools/clang_tools/support:BUILD.prebuilt"), "BUILD")
-
-_clang_tools = repository_rule(
-    implementation = _clang_tools_impl,
-    environ = ["LLVM_CONFIG"],
-)
-
 def envoy_dependencies(skip_targets = []):
+    # Setup Envoy developer tools.
+    envoy_dev_binding()
+
     # Treat Envoy's overall build config as an external repo, so projects that
     # build Envoy as a subcomponent can easily override the config.
     if "envoy_build_config" not in native.existing_rules().keys():
@@ -129,8 +112,6 @@ def envoy_dependencies(skip_targets = []):
         name = "ssl",
         actual = "@envoy//bazel:boringssl",
     )
-
-    _clang_tools(name = "clang_tools")
 
     # The long repo names (`com_github_fmtlib_fmt` instead of `fmtlib`) are
     # semi-standard in the Bazel community, intended to avoid both duplicate

tools/clang_tools/syntax_only/BUILD
@@ -1,4 +1,4 @@
-load("//tools/clang_tools/support:clang_tools.bzl", "envoy_clang_tools_cc_binary")
+load("//clang_tools/support:clang_tools.bzl", "envoy_clang_tools_cc_binary")
 
 licenses(["notice"])  # Apache 2
 