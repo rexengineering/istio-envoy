rexengineering/istio-envoy

commit 5ca58319e4781c9097e7b9cfa4082aaa6deaf1cd

[buffer] Change Buffer::getRawSlices API to improve performance and usability. (#10381)
* Change Buffer::getRawSlices API to improve performance and usability.

Signed-off-by: Antonio Vicente <avd@google.com>

source/common/buffer/buffer_impl.cc
@@ -44,10 +44,7 @@ void OwnedImpl::add(absl::string_view data) { add(data.data(), data.size()); }
 
 void OwnedImpl::add(const Instance& data) {
   ASSERT(&data != this);
-  uint64_t num_slices = data.getRawSlices(nullptr, 0);
-  absl::FixedArray<RawSlice> slices(num_slices);
-  data.getRawSlices(slices.begin(), num_slices);
-  for (const RawSlice& slice : slices) {
+  for (const RawSlice& slice : data.getRawSlices()) {
     add(slice.mem_, slice.len_);
   }
 }
@@ -169,23 +166,26 @@ void OwnedImpl::drain(uint64_t size) {
   }
 }
 
-uint64_t OwnedImpl::getRawSlices(RawSlice* out, uint64_t out_size) const {
-  uint64_t num_slices = 0;
+RawSliceVector OwnedImpl::getRawSlices(absl::optional<uint64_t> max_slices) const {
+  uint64_t max_out = slices_.size();
+  if (max_slices.has_value()) {
+    max_out = std::min(max_out, max_slices.value());
+  }
+
+  RawSliceVector raw_slices;
+  raw_slices.reserve(max_out);
   for (const auto& slice : slices_) {
+    if (raw_slices.size() >= max_out) {
+      break;
+    }
+
     if (slice->dataSize() == 0) {
       continue;
     }
-    if (num_slices < out_size) {
-      out[num_slices].mem_ = slice->data();
-      out[num_slices].len_ = slice->dataSize();
-    }
-    // Per the definition of getRawSlices in include/envoy/buffer/buffer.h, we need to return
-    // the total number of slices needed to access all the data in the buffer, which can be
-    // larger than out_size. So we keep iterating and counting non-empty slices here, even
-    // if all the caller-supplied slices have been filled.
-    num_slices++;
+
+    raw_slices.emplace_back(RawSlice{slice->data(), slice->dataSize()});
   }
-  return num_slices;
+  return raw_slices;
 }
 
 uint64_t OwnedImpl::length() const {
@@ -470,9 +470,8 @@ bool OwnedImpl::startsWith(absl::string_view data) const {
 
 Api::IoCallUint64Result OwnedImpl::write(Network::IoHandle& io_handle) {
   constexpr uint64_t MaxSlices = 16;
-  RawSlice slices[MaxSlices];
-  const uint64_t num_slices = std::min(getRawSlices(slices, MaxSlices), MaxSlices);
-  Api::IoCallUint64Result result = io_handle.writev(slices, num_slices);
+  RawSliceVector slices = getRawSlices(MaxSlices);
+  Api::IoCallUint64Result result = io_handle.writev(slices.begin(), slices.size());
   if (result.ok() && result.rc_ > 0) {
     drain(static_cast<uint64_t>(result.rc_));
   }
@@ -488,16 +487,9 @@ OwnedImpl::OwnedImpl(const Instance& data) : OwnedImpl() { add(data); }
 OwnedImpl::OwnedImpl(const void* data, uint64_t size) : OwnedImpl() { add(data, size); }
 
 std::string OwnedImpl::toString() const {
-  uint64_t num_slices = getRawSlices(nullptr, 0);
-  absl::FixedArray<RawSlice> slices(num_slices);
-  getRawSlices(slices.begin(), num_slices);
-  size_t len = 0;
-  for (const RawSlice& slice : slices) {
-    len += slice.len_;
-  }
   std::string output;
-  output.reserve(len);
-  for (const RawSlice& slice : slices) {
+  output.reserve(length());
+  for (const RawSlice& slice : getRawSlices()) {
     output.append(static_cast<const char*>(slice.mem_), slice.len_);
   }
 

include/envoy/buffer/BUILD
@@ -11,6 +11,9 @@ envoy_package()
 envoy_cc_library(
     name = "buffer_interface",
     hdrs = ["buffer.h"],
+    external_deps = [
+        "abseil_inlined_vector",
+    ],
     deps = [
         "//include/envoy/api:os_sys_calls_interface",
         "//include/envoy/network:io_handle_interface",

include/envoy/buffer/buffer.h
@@ -13,7 +13,9 @@
 
 #include "common/common/byte_order.h"
 
+#include "absl/container/inlined_vector.h"
 #include "absl/strings/string_view.h"
+#include "absl/types/optional.h"
 
 namespace Envoy {
 namespace Buffer {
@@ -27,6 +29,8 @@ struct RawSlice {
 
   bool operator==(const RawSlice& rhs) const { return mem_ == rhs.mem_ && len_ == rhs.len_; }
 };
+
+using RawSliceVector = absl::InlinedVector<RawSlice, 16>;
 
 /**
  * A wrapper class to facilitate passing in externally owned data to a buffer via addBufferFragment.
@@ -124,14 +128,12 @@ public:
   virtual void drain(uint64_t size) PURE;
 
   /**
-   * Fetch the raw buffer slices. This routine is optimized for performance.
-   * @param out supplies an array of RawSlice objects to fill.
-   * @param out_size supplies the size of out.
-   * @return the actual number of slices needed, which may be greater than out_size. Passing
-   *         nullptr for out and 0 for out_size will just return the size of the array needed
-   *         to capture all of the slice data.
+   * Fetch the raw buffer slices.
+   * @param max_slices supplies an optional limit on the number of slices to fetch, for performance.
+   * @return RawSliceVector with non-empty slices in the buffer.
    */
-  virtual uint64_t getRawSlices(RawSlice* out, uint64_t out_size) const PURE;
+  virtual RawSliceVector
+  getRawSlices(absl::optional<uint64_t> max_slices = absl::nullopt) const PURE;
 
   /**
    * @return uint64_t the total length of the buffer (not necessarily contiguous in memory).

source/common/access_log/access_log_manager_impl.cc
@@ -95,9 +95,7 @@ AccessLogFileImpl::~AccessLogFileImpl() {
 }
 
 void AccessLogFileImpl::doWrite(Buffer::Instance& buffer) {
-  uint64_t num_slices = buffer.getRawSlices(nullptr, 0);
-  absl::FixedArray<Buffer::RawSlice> slices(num_slices);
-  buffer.getRawSlices(slices.begin(), num_slices);
+  Buffer::RawSliceVector slices = buffer.getRawSlices();
 
   // We must do the actual writes to disk under lock, so that we don't intermix chunks from
   // different AccessLogFileImpl pointing to the same underlying file. This can happen either via

source/common/buffer/buffer_impl.h
@@ -519,7 +519,7 @@ public:
   void commit(RawSlice* iovecs, uint64_t num_iovecs) override;
   void copyOut(size_t start, uint64_t size, void* data) const override;
   void drain(uint64_t size) override;
-  uint64_t getRawSlices(RawSlice* out, uint64_t out_size) const override;
+  RawSliceVector getRawSlices(absl::optional<uint64_t> max_slices = absl::nullopt) const override;
   uint64_t length() const override;
   void* linearize(uint32_t size) override;
   void move(Instance& rhs) override;

source/common/buffer/zero_copy_input_stream_impl.cc
@@ -25,10 +25,10 @@ bool ZeroCopyInputStreamImpl::Next(const void** data, int* size) {
     position_ = 0;
   }
 
-  Buffer::RawSlice slice;
-  const uint64_t num_slices = buffer_->getRawSlices(&slice, 1);
+  Buffer::RawSliceVector slices = buffer_->getRawSlices(1);
 
-  if (num_slices > 0 && slice.len_ > 0) {
+  if (slices.size() > 0 && slices[0].len_ > 0) {
+    auto& slice = slices[0];
     *data = slice.mem_;
     *size = slice.len_;
     position_ = slice.len_;

source/common/common/base64.cc
@@ -194,13 +194,9 @@ std::string Base64::encode(const Buffer::Instance& buffer, uint64_t length) {
   std::string ret;
   ret.reserve(output_length);
 
-  uint64_t num_slices = buffer.getRawSlices(nullptr, 0);
-  absl::FixedArray<Buffer::RawSlice> slices(num_slices);
-  buffer.getRawSlices(slices.begin(), num_slices);
-
   uint64_t j = 0;
   uint8_t next_c = 0;
-  for (const Buffer::RawSlice& slice : slices) {
+  for (const Buffer::RawSlice& slice : buffer.getRawSlices()) {
     const uint8_t* slice_mem = static_cast<const uint8_t*>(slice.mem_);
 
     for (uint64_t i = 0; i < slice.len_ && j < length; ++i, ++j) {

source/common/compressor/zlib_compressor_impl.cc
@@ -38,11 +38,7 @@ void ZlibCompressorImpl::init(CompressionLevel comp_level, CompressionStrategy c
 uint64_t ZlibCompressorImpl::checksum() { return zstream_ptr_->adler; }
 
 void ZlibCompressorImpl::compress(Buffer::Instance& buffer, State state) {
-  const uint64_t num_slices = buffer.getRawSlices(nullptr, 0);
-  absl::FixedArray<Buffer::RawSlice> slices(num_slices);
-  buffer.getRawSlices(slices.begin(), num_slices);
-
-  for (const Buffer::RawSlice& input_slice : slices) {
+  for (const Buffer::RawSlice& input_slice : buffer.getRawSlices()) {
     zstream_ptr_->avail_in = input_slice.len_;
     zstream_ptr_->next_in = static_cast<Bytef*>(input_slice.mem_);
     // Z_NO_FLUSH tells the compressor to take the data in and compresses it as much as possible

commit 2e778994fb9a98035ba7a389c426581be1fa5a94

[buffer] Add temporary static_cast<size_t> when constructing RawSlices in Buffer::OwnedImpl to fix 32-bit Envoy mobile builds. (#10510)
Description: Add static_cast<size_t> to code that constructs RawSlice from BufferImpl Slices to fix 32-bit Envoy build broken by #10381. This should be a no-op for 64-bit platforms where both uint64_t and size_t are 64-bit unsigned types. It is safe in 32-bit platforms because the value being cast is a size of a contiguous buffer and is therefore guaranteed to fit in size_t.
Risk Level: n/a
Testing: n/a
Docs Changes: n/a
Release Notes: n/a

Signed-off-by: Antonio Vicente <avd@google.com>

source/common/buffer/buffer_impl.cc
@@ -183,7 +183,13 @@ RawSliceVector OwnedImpl::getRawSlices(absl::optional<uint64_t> max_slices) cons
       continue;
     }
 
-    raw_slices.emplace_back(RawSlice{slice->data(), slice->dataSize()});
+    // Temporary cast to fix 32-bit Envoy mobile builds, where sizeof(uint64_t) != sizeof(size_t).
+    // dataSize represents the size of a buffer so size_t should always be large enough to hold its
+    // size regardless of architecture. Buffer slices should in practice be relatively small, but
+    // there is currently no max size validation.
+    // TODO(antoniovicente) Set realistic limits on the max size of BufferSlice and consider use of
+    // size_t instead of uint64_t in the Slice interface.
+    raw_slices.emplace_back(RawSlice{slice->data(), static_cast<size_t>(slice->dataSize())});
   }
   return raw_slices;
 }